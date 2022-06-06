c74746857f037e04e0da2f81fa1e96a11b17bff9
TsugawaNaoki0 <104245092+TsugawaNaoki0@users.noreply.github.com>
2022-06-06 14:23:54 +0900

    Add files via upload

---
+++ reader.py
@@ -0,0 +1,23 @@
+new_line = []
+base_line = "<p>投稿者:</p><p>内容:</p><p></p><input type='button' name='' value='削除'>"
+
+with open('test.txt') as f:
+    line = f.readline()
+    while line:
+        line = line.rstrip()
+        # print(line)
+        if((base_line in line) == False):
+            # print("XXX")
+            new_line.append(line)
+
+        line = f.readline()
+
+print()
+for i in range(len(new_line)):
+    print(new_line[i])
+
+f = open('test.txt', 'w')
+
+f.writelines(new_line)
+
+f.close()
